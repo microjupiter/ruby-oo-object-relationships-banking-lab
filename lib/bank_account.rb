af5afe1eed8313a8f4f8b12bd24a24710e8ed325
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-23 17:22:53 -0600

    commit tree 8f7b7452f9d510fe648b277ddd94ae8fe0dee2a3

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,56 @@
 class BankAccount
+    attr_reader :name, :status
+    attr_accessor :balance, :status
 
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(deposit_amount)
+        self.balance += deposit_amount
+    end
+
+    def display_balance
+        "Your balance is $#{balance}."
+    end
+
+    
+
+    def valid?
+        balance > 0 && status == "open"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
+
+# class BankAccount
+#     attr_reader :name
+#     attr_accessor :balance, :status
+#     def initialize(name)
+#         @name = name
+#         @balance = 1000
+#         @status = "open"
+#     end
+#     def deposit(amount)
+#         self.balance += amount
+#     end
+#     def display_balance
+#         "Your balance is $#{balance}."
+#     end
+#     def close_account
+#         self.status = "closed"
+#     end
+#     def valid?
+#         if @balance <= 0
+#             false
+#         elsif @status == "closed"
+#             false
+#         else
+#             true
+#     end
+#     end
+#     end
